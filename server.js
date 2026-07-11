ca1eb045da006d40748845dd4b5f5295cbc3fc42
hiru616 <ishakyaranhiru@gmail.com>
2026-07-11 13:02:26 +0530

    Add All the API contracts

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./openapi.yaml');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +12,7 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json()); // Parses incoming JSON requests
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Basic Health Check Route
 app.get('/api/health', (req, res) => {
